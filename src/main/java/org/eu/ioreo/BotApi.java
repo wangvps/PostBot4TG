d5669ff54a0f10da35996050dc1a047ce0e93823
UniOreoX <admin@ioreo.eu.org>
2022-08-27 15:29:31 +0800

    first commit

---
+++ src/main/java/org/eu/ioreo/BotApi.java
@@ -0,0 +1,7 @@
+package org.eu.ioreo;
+
+public class BotApi {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/java/org/eu/ioreo/PostBot.java
@@ -0,0 +1,5 @@
+package org.eu.ioreo;
+
+public class PostBot {
+    
+}
